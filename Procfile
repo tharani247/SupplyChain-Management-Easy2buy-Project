a45f1f476586adb25e39251eb6c0fabf86b07488
tharani247 <tharanivadde247@gmail.com>
2021-05-23 01:53:49 -0700

    deploying functionality

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myecommerce.wsgi
--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +156,6 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
